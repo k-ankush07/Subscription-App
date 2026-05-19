f2441875ab7de579e93d4c881dd26f29212c6bf7
k-ankush07 <ankush@ecombithub.com>
2026-05-20 00:58:38 +0530

    add app.index try to check polaris

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-
+import {Page, Grid, LegacyCard} from '@shopify/polaris';
+import {MediaCard} from '@shopify/polaris';
+import {Thumbnail} from '@shopify/polaris';
 function Index() {
   return (
-    <div>
-      
-    </div>
+   <Page fullWidth>
+         <MediaCard
+      title="Getting Started"
+      primaryAction={{
+        content: 'Learn about getting started',
+        onAction: () => {},
+      }}
+      description="Discover how Shopify can power up your entrepreneurial journey."
+      popoverActions={[{content: 'Dismiss', onAction: () => {}}]}
+    >
+      <img
+        alt=""
+        width="100%"
+        height="100%"
+        style={{
+          objectFit: 'cover',
+          objectPosition: 'center',
+        }}
+        src="https://burst.shopifycdn.com/photos/business-woman-smiling-in-office.jpg?width=1850"
+      />
+    </MediaCard>
+    </Page>
   )
 }
 
