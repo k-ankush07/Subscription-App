ceaf4cf6b287175f22ed02bf4b77866f4e0ccbc9
k-ankush07 <ankush@ecombithub.com>
2026-05-20 01:07:37 +0530

    save bar

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
-import {Page, Grid,LegacyCard, Scrollable} from '@shopify/polaris';
+import {Frame, ContextualSaveBar, Page} from '@shopify/polaris';
 function Index() {
   return (
    <Page fullWidth>
-        <LegacyCard title="Terms of service" sectioned>
-      <Scrollable shadow style={{height: '200px'}} >
-        <p>
-          By signing up for the Shopify service (“Service”) or any of the
-          services of Shopify Inc. (“Shopify”) you are agreeing to be bound by
-          the following terms and conditions (“Terms of Service”). The Services
-          offered by Shopify under the Terms of Service include various products
-          and services to help you create and manage a retail store, whether an
-          online store (“Online Services”), a physical retail store (“POS
-          Services”), or both. Any new features or tools which are added to the
-          current Service shall be also subject to the Terms of Service. You can
-          review the current version of the Terms of Service at any time at
-          https://www.shopify.com/legal/terms. Shopify reserves the right to
-          update and change the Terms of Service by posting updates and changes
-          to the Shopify website. You are advised to check the Terms of Service
-          from time to time for any updates or changes that may impact you.By signing up for the Shopify service (“Service”) or any of the services of Shopify Inc. (“Shopify”) you are agreeing to be bound by the following terms and conditions (“Terms of Service”). The Services offered by Shopify under the Terms of Service include various products and services to help you create and manage a retail store, whether an online store (“Online Services”), a physical retail store (“POS Services”), or both. Any new features or tools which are added to the current Service shall be also subject to the Terms of Service. You can review the current version of the Terms of Service at any time at https://www.shopify.com/legal/terms. Shopify reserves the right to update and change the Terms of Service by posting updates and changes to the Shopify website. You are advised to check the Terms of Service from time to time for any updates or changes that may impact you.By signing up for the Shopify service (“Service”) or any of the services of Shopify Inc. (“Shopify”) you are agreeing to be bound by the following terms and conditions (“Terms of Service”). The Services offered by Shopify under the Terms of Service include various products and services to help you create and manage a retail store, whether an online store (“Online Services”), a physical retail store (“POS Services”), or both. Any new features or tools which are added to the current Service shall be also subject to the Terms of Service. You can review the current version of the Terms of Service at any time at https://www.shopify.com/legal/terms. Shopify reserves the right to update and change the Terms of Service by posting updates and changes to the Shopify website. You are advised to check the Terms of Service from time to time for any updates or changes that may impact you.By signing up for the Shopify service (“Service”) or any of the services of Shopify Inc. (“Shopify”) you are agreeing to be bound by the following terms and conditions (“Terms of Service”). The Services offered by Shopify under the Terms of Service include various products and services to help you create and manage a retail store, whether an online store (“Online Services”), a physical retail store (“POS Services”), or both. Any new features or tools which are added to the current Service shall be also subject to the Terms of Service. You can review the current version of the Terms of Service at any time at https://www.shopify.com/legal/terms. Shopify reserves the right to update and change the Terms of Service by posting updates and changes to the Shopify website. You are advised to check the Terms of Service from time to time for any updates or changes that may impact you.
-        </p>
-      </Scrollable>
-    </LegacyCard>
+      <div style={{height: '250px'}}>
+      <Frame
+        logo={{
+          width: 86,
+          contextualSaveBarSource:
+            'https://cdn.shopify.com/s/files/1/2376/3301/files/Shopify_Secondary_Inverted.png',
+        }}
+      >
+        <ContextualSaveBar
+          message="Unsaved changes"
+          saveAction={{
+            onAction: () => console.log('add form submit logic'),
+            loading: false,
+            disabled: false,
+          }}
+          discardAction={{
+            onAction: () => console.log('add clear form logic'),
+          }}
+        />
+      </Frame>
+    </div>
     </Page>
   )
 }
