1ac92af009a6977535c0803cae853faaf1e10e9e
k-ankush07 <ankush@ecombithub.com>
2026-06-12 18:57:42 +0530

    code

--- extensions/subscription-app/assets/subscription.js
+++ extensions/subscription-app/assets/subscription.js
@@ -1,3 +1,55 @@
+// (function () {
+//   var widget = document.getElementById('subscription-widget');
+//   if (!widget) return; 
+
+//   var radios = widget.querySelectorAll('input[name="purchase_type"]');
+//   var plansContainer = document.getElementById('selling-plans-container');
+//   var planSelect = document.getElementById('selling-plan-select');
+//   var addToCartBtn = document.querySelector('[name="add"]') 
+//                   || document.querySelector('.product-form__submit');
+                  
+
+//   // Show/hide plan dropdown based on selected radio
+//   radios.forEach(function (radio) {
+//     radio.addEventListener('change', function () {
+//       if (this.value === 'subscription') {
+//         plansContainer.style.display = 'block';
+//         setSellingPlan(planSelect.value);
+//       } else {
+//         plansContainer.style.display = 'none';
+//         clearSellingPlan();
+//       }
+//     });
+//   });
+
+//   // When plan dropdown changes
+//   planSelect.addEventListener('change', function () {
+//     setSellingPlan(this.value);
+//   });
+
+//   function setSellingPlan(planId) {
+
+//     // Remove old hidden input if exists
+//     clearSellingPlan();
+//      var numericId = String(planId).split('/').pop();
+//      console.log("numve", numericId)
+//     var input = document.createElement('input');
+//     input.type = 'hidden';
+//     input.name = 'selling_plan';
+//     input.id = 'selling-plan-input';
+//     // input.value = numericId; 
+//     input.value = planId;
+//     var form = document.querySelector('form[action="/cart/add"]');
+//     if (form) form.appendChild(input);
+//     //  console.log("form:", form); 
+//   }
+
+//   function clearSellingPlan() {
+//     var old = document.getElementById('selling-plan-input');
+//     if (old) old.remove();
+//   }
+
+// })();
 
 
 (function () {
