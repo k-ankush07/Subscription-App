ba66776bebf48014de7e3a50af0644bf879036ec
k-ankush07 <ankush@ecombithub.com>
2026-06-15 16:41:12 +0530

    find out the image

--- app/routes/app.subscription.$subId.jsx
+++ app/routes/app.subscription.$subId.jsx
@@ -65,6 +65,7 @@ export async function loader({ request, params }) {
             node {
               id
               title
+              
               quantity
               currentPrice { amount currencyCode }
               lineDiscountedPrice { amount currencyCode }
