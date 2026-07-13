4cbc83de340da72d62b53f76b65c956bbcc0a528
k-ankush07 <ankush@ecombithub.com>
2026-07-13 18:30:12 +0530

    add

--- app/routes/app.plans.jsx
+++ app/routes/app.plans.jsx
@@ -191,7 +191,7 @@ export const action = async ({ request }) => {
 
   return Response.json({ success: true });
 };
-// bdjsfjjhbjhdsbhj
+// bdjsfjjhbjhdsbhjiuluuil
 function Plans() {
   const { plans } = useLoaderData();
   const navigate = useNavigate();
