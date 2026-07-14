6977ef0dc0aa886389971e8f153c7346f01c1188
k-ankush07 <ankush@ecombithub.com>
2026-07-14 18:54:20 +0530

    add

--- app/routes/api.process-billing-cycles.jsx
+++ app/routes/api.process-billing-cycles.jsx
@@ -735,7 +735,7 @@ export const action = async ({ request }) => {
 
   const url = new URL(request.url);
   const shopOverride = url.searchParams.get("shop");
-// sdsfdfergergoioio[[]]iuiuopiiouittyjtujuiouilliuhj
+// sdsfdfergergoioio[[]]iuiuopiiouittyjtuj
   try {
     let shops = await getShopsWithOfflineTokens();
     if (shopOverride) {
