a8f624b9454a825da610ec779d8f2e04c0a875c7
k-ankush07 <ankush@ecombithub.com>
2026-07-17 18:38:26 +0530

    ading

--- app/lib/billing-preview.server.js
+++ app/lib/billing-preview.server.js
@@ -905,7 +905,7 @@ async function getContractPreview(admin, contractId) {
         firstLine?.currentPrice?.currencyCode,
     };
   }
-// jkddbvbfvbufdbvf
+// jkddbvbfvbufdbvfdwdw
   const quantityAction = Array.isArray(actionsForNextCycle)
     ? actionsForNextCycle.find((a) => a.type === "QUANTITY_CHANGE")
     : null;
