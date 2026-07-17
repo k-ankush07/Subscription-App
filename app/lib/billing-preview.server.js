107574e24fa6fc88c4fb407a67276b708431260d
k-ankush07 <ankush@ecombithub.com>
2026-07-17 18:45:18 +0530

    remove comment line

--- app/lib/billing-preview.server.js
+++ app/lib/billing-preview.server.js
@@ -905,7 +905,7 @@ async function getContractPreview(admin, contractId) {
         firstLine?.currentPrice?.currencyCode,
     };
   }
-// jkddbvbfvbufdbvfdwewddqw3dswdwdddd
+
   const quantityAction = Array.isArray(actionsForNextCycle)
     ? actionsForNextCycle.find((a) => a.type === "QUANTITY_CHANGE")
     : null;
