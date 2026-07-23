feac7e55d907eda36ec18128123134f3dfcfa52c
k-ankush07 <ankush@ecombithub.com>
2026-07-23 18:23:14 +0530

    ading

--- app/lib/billing-preview.server.js
+++ app/lib/billing-preview.server.js
@@ -679,7 +679,7 @@ async function findBlockingBillingCycleIndex(admin, contractId, aroundDate = new
 
   return cycle.cycleIndex;
 }
-// fefefdsfdedcs
+
 async function applyActionsToCycle(
   admin,
   contractId,
