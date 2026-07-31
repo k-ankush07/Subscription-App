0eb9fb71962d8531761fbbc20ccf4461b13e104d
k-ankush07 <ankush@ecombithub.com>
2026-07-31 18:41:18 +0530

    remove code

--- app/routes/components/createSubscription.jsx
+++ app/routes/components/createSubscription.jsx
@@ -298,7 +298,7 @@ function CreateSubscription({ currencyCode }) {
               )}
             </>
           )}
-        </div>dsddwdewdewddeedfewfdfdewfedfsdxsxdasd
+        </div>dsddwdewdewddeedfewfdfdewfedfsdxsxdasddxefd
 {/* fdferfedwsdsdsdswwdwdwsddesdsafsdewdedeefwdwddsddsdefeffdecdewedwdddwdwede fcdfdsfddxsdsadsadsaewdsdseddesdcsefdef*/}
       </Card>
     </Page>
