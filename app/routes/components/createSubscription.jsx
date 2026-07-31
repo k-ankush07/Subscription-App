d88bc68e1a1c49c54ad842acfa549fc2b023bf52
k-ankush07 <ankush@ecombithub.com>
2026-07-31 18:34:50 +0530

    code add

--- app/routes/components/createSubscription.jsx
+++ app/routes/components/createSubscription.jsx
@@ -299,7 +299,7 @@ function CreateSubscription({ currencyCode }) {
             </>
           )}
         </div>
-{/* fdferfedwsdsdsdswwdwdwsddesdsafsefwdwddsdf */}
+{/* fdferfedwsdsdsdswwdwdwsddesdsafsefwdwddsdf desdcsefdef*/}
       </Card>
     </Page>
   )
