d709b0369df14503d5813f158243eedff1e2a178
k-ankush07 <ankush@ecombithub.com>
2026-07-31 18:38:21 +0530

    push code files

--- app/routes/components/createSubscription.jsx
+++ app/routes/components/createSubscription.jsx
@@ -299,7 +299,7 @@ function CreateSubscription({ currencyCode }) {
             </>
           )}
         </div>
-{/* fdferfedwsdsdsdswwdwdwsddesdsafsefwdwddsddsdefeffdecdewedwdddwdwede dxsdsadsadsaewdsdseddesdcsefdef*/}
+{/* fdferfedwsdsdsdswwdwdwsddesdsafsefwdwddsddsdefeffdecdewedwdddwdwede fcdfdsfddxsdsadsadsaewdsdseddesdcsefdef*/}
       </Card>
     </Page>
   )
