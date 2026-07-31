9b4340c6c3aa4e5edf34116f67a3fc32c65cc4bd
k-ankush07 <ankush@ecombithub.com>
2026-07-31 18:43:31 +0530

    update

--- app/routes/components/createSubscription.jsx
+++ app/routes/components/createSubscription.jsx
@@ -298,7 +298,7 @@ function CreateSubscription({ currencyCode }) {
               )}
             </>
           )}
-        </div>dsddwdewdewddeedfewfdfdewfedfsdxsxdasddxefdfewdfedeefcsdfdsdvfvfdvdfdsfvdfr
+        </div>dsddwdewdewddeedfewfdfdewfedfsdxsxdasddxefdfewdfedeefcsdfdsdvfvfdvdfdsfvdfredfdsfdsfr
 {/* fdferfedwsdsdsdswwdwdwsddesdsafsdewdedeefwdwddsddsdefeffdecdewedwdddwdwede fcdfdsfddxsdsadsadsaewdsdseddesdcsefdef*/}
       </Card>
     </Page>
