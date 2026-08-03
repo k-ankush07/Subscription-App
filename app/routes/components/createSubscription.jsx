73d98bf313f5945b0c0fc8ef74f75744d85f0a04
k-ankush07 <ankush@ecombithub.com>
2026-08-03 18:12:40 +0530

    remove icon

--- app/routes/components/createSubscription.jsx
+++ app/routes/components/createSubscription.jsx
@@ -468,7 +468,7 @@ function CreateSubscription({ currencyCode, shop }) {
           />
           
         </div>
-{/* dsdsdsjsjs */}fefcdscfdwdwdewdewwddddsadsadcsadsdxdwdwdsdzdwdsdzdscdsfcsdcf
+{/* dsdsdsjsjs */}fefcdscfdwdwdewdewwddddsadsadcsadsdxdwdwdsdzdwdsdzdscdsfcsdcfdsdssd
         <div>
           <h2>Subscription orders</h2>
           <TextField
