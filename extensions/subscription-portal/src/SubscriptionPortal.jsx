d71d9e39be3f7af4d97bf379fae65b4144c2d52a
k-ankush07 <ankush@ecombithub.com>
2026-08-03 17:44:30 +0530

    url chnage

--- extensions/subscription-portal/src/SubscriptionPortal.jsx
+++ extensions/subscription-portal/src/SubscriptionPortal.jsx
@@ -2,7 +2,7 @@ import '@shopify/ui-extensions/preact';
 import { render } from "preact";
 import { useState, useEffect, useCallback, useRef } from "preact/hooks";
 import { hideModalById, showModalById } from "./Modalutils";
-const API_BASE = "https://rim-suddenly-angle-math.trycloudflare.com";
+const API_BASE = "https://mentor-solid-adrian-dealt.trycloudflare.com";
 
 export default async () => {
     render(<Extension />, document.body);
