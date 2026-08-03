09ac14782b88960cdf52ca13865b9f0c4ba5495e
k-ankush07 <ankush@ecombithub.com>
2026-08-03 18:16:42 +0530

    remove function

--- app/routes/components/createSubscription.jsx
+++ app/routes/components/createSubscription.jsx
@@ -468,7 +468,7 @@ function CreateSubscription({ currencyCode, shop }) {
           />
           
         </div>
-djndjnksncssdsddwdsdwdwsdsdsddsdfcdsf
+djndjnksncssdsddwdsdwdwsdsdsddsdfcdsfsddwdw
         <div>
           <h2>Subscription orders</h2>
           <TextField
