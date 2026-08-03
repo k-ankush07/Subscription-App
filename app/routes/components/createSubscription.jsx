d418c07d71ee8d45ff9f0365becfae231b5ea3af
k-ankush07 <ankush@ecombithub.com>
2026-08-03 18:07:35 +0530

    code new

--- app/routes/components/createSubscription.jsx
+++ app/routes/components/createSubscription.jsx
@@ -468,7 +468,7 @@ function CreateSubscription({ currencyCode, shop }) {
           />
           
         </div>
-?kefefjejfbjefbjewbfhecedrgrfgvfgvfgvsddwdwdwedwddddewdewdes
+?kefefjejfbjefbjewbfhecedrgrfgvfgvfgvsddwdwdwedwddddewdewdescxcxcxc
         <div>
           <h2>Subscription orders</h2>
           <TextField
