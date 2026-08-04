1ec475544ac9a89483fb113d077dc6a0a9be8908
k-ankush07 <ankush@ecombithub.com>
2026-08-04 18:52:28 +0530

    remove text

--- app/routes/app.create-contract.jsx
+++ app/routes/app.create-contract.jsx
@@ -85,7 +85,7 @@ export async function loader({ request }) {
     shop: session.shop,
   };
 }
-?jsdjdjdjjdjjcjcbsdcxvcxzvzxwscszczcwsdcszdsaaaaaaaaafszfwacscfsafwwesdacfsfesfwdwedesaafcsaefqdssa
+?jsdjdjdjjdjjcjcbsdcxvcxzvzxwscszczcwsdcszdsaaaaaaaaafszfwacscfsafwwesdacfsfesfwdwedesaafcsaefqdssasadcsz
 export async function action({ request }) {
   const { session } = await authenticate.admin(request);
   const formData = await request.formData();
