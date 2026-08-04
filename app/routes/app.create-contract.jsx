352949fa7a4f52d6a5dcf7e9fc450aa4f0519539
k-ankush07 <ankush@ecombithub.com>
2026-08-04 18:51:39 +0530

    files code

--- app/routes/app.create-contract.jsx
+++ app/routes/app.create-contract.jsx
@@ -85,7 +85,7 @@ export async function loader({ request }) {
     shop: session.shop,
   };
 }
-?jsdjdjdjjdjjcjcbsdcxvcxzvzxwscszczcwsdcszdsaaaaaaaaafszfwacscfsafwwesdacfsfesfw
+?jsdjdjdjjdjjcjcbsdcxvcxzvzxwscszczcwsdcszdsaaaaaaaaafszfwacscfsafwwesdacfsfesfwdwedesaafcsaef
 export async function action({ request }) {
   const { session } = await authenticate.admin(request);
   const formData = await request.formData();
