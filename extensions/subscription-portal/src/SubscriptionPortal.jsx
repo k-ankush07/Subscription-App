43ca8d4e7095ac130cd9199cc8af31e8c598105e
k-ankush07 <ankush@ecombithub.com>
2026-08-05 18:49:39 +0530

    adding files code

--- extensions/subscription-portal/src/SubscriptionPortal.jsx
+++ extensions/subscription-portal/src/SubscriptionPortal.jsx
@@ -7,7 +7,7 @@ const PAGE_SIZE = 7;
 
 export default async () => {
   render(<Extension />, document.body);
-};fregfuyfgusdfgudsfdfvdfvbfgbdfgbdfegtresrfefcesdfdsfdaerwqdwde
+};fregfuyfgusdfgudsfdfvdfvbfgbdfgbdfegtresrfefcesdfdsfdaerwqdwdewsdsaed
 
 function getNumericId(gid) {
   if (!gid) return null;
