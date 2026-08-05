14c0ed628da04622fb48c3545dafe5ae933b7c67
k-ankush07 <ankush@ecombithub.com>
2026-08-05 18:51:45 +0530

    url

--- extensions/subscription-portal/src/SubscriptionPortal.jsx
+++ extensions/subscription-portal/src/SubscriptionPortal.jsx
@@ -8,7 +8,7 @@ const PAGE_SIZE = 7;
 export default async () => {
   render(<Extension />, document.body);
 }
-
+dfvrdfgregteef
 function getNumericId(gid) {
   if (!gid) return null;
   return gid.split("/").pop();
