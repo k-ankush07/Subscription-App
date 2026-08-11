9d5df8c791599ce6d57b537acfaebfc169156e1a
k-ankush07 <ankush@ecombithub.com>
2026-08-11 18:29:06 +0530

    reason

--- app/routes/app.cancelSubscription.jsx
+++ app/routes/app.cancelSubscription.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import PortalNav from "./components/PortalNav";
+import { BlockStack, Page } from "@shopify/polaris";
 function CancelSubscription() {
   return (
-    <div> hello</div>
-  )
+    <Page title="Cancellation reason">
+      <BlockStack gap="400">
+        <PortalNav />
+      </BlockStack>
+    </Page>
+  );
 }
 
-export default CancelSubscription
+export default CancelSubscription;
