b26edd1c91f3a13cbf9171c76135f938f35cfc5e
k-ankush07 <ankush@ecombithub.com>
2026-08-19 16:24:18 +0530

    chnage url

--- app/routes/cron.server.js
+++ app/routes/cron.server.js
@@ -7,9 +7,9 @@ export function startBillingCycleCron() {
   globalThis.__billingCronStarted = true;
   cron.schedule("*/5 * * * *", async () => {
     console.log("[cron] Running process-billing-cycles...");
-    console.log("process.env https://early-finding-thinkpad-guarantee.trycloudflare.com")
+    console.log("process.env https://lead-freely-brilliant-concerned.trycloudflare.com")
     try {
-      const res = await fetch(`https://early-finding-thinkpad-guarantee.trycloudflare.com/api/process-billing-cycles`, {
+      const res = await fetch(`https://lead-freely-brilliant-concerned.trycloudflare.com/api/process-billing-cycles`, {
         method: "POST",
         headers: { "x-cron-secret": process.env.CRON_SECRET },
       });
