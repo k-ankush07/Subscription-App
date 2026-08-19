118cbe38d6916275f546e652632b0e67b23b53be
k-ankush07 <ankush@ecombithub.com>
2026-08-19 15:02:22 +0530

    change url

--- extensions/subscription-portal/src/SubscriptionPortal.jsx
+++ extensions/subscription-portal/src/SubscriptionPortal.jsx
@@ -3,7 +3,7 @@ import { render } from "preact";
 import { useState, useEffect, useCallback, useRef } from "preact/hooks";
 import { COUNTRIES } from "../../../app/routes/utils/countries";
 
-const API_BASE = "https://filme-located-mailed-open.trycloudflare.com";
+const API_BASE = "https://early-finding-thinkpad-guarantee.trycloudflare.com";
 const PAGE_SIZE = 7;
 const CANCEL_REASONS = [
   "Too expensive",
