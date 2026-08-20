48faba96efc3c46ddd7df82e7d87a199dedebe82
k-ankush07 <ankush@ecombithub.com>
2026-08-20 15:09:36 +0530

    push code

--- app/routes/app.widgets.jsx
+++ app/routes/app.widgets.jsx
@@ -1,42 +1,69 @@
-import { Badge, Card, Page, Button, Icon } from "@shopify/polaris";
+import { Badge, Card, Page, Button } from "@shopify/polaris";
 import { PlusCircleIcon } from "@shopify/polaris-icons";
-import React from 'react'
+import React from "react";
 
-function widgets() {
+function Widgets() {
   return (
-    <Page
-    
-      primaryAction={{ content: "Create widget" ,icon: PlusCircleIcon,}}
-    >
-      <Card
-      title='Widgets'
-      >
-        <table border="1" style={{ width: "100%", borderCollapse: "collapse" }}>
+    <Page>
+      <Card>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignContent:"center",
+            padding: " 10px 0px",
+          }}
+        >
+          <h2
+            style={{
+              margin: 0,
+              fontSize: "20px",
+              fontWeight:"bold",
+            }}
+          >
+            Widgets
+          </h2>
+
+          <Button variant="primary" icon={PlusCircleIcon}>
+            Create widget
+          </Button>
+        </div>
+
+        <table
+          border="1"
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+          }}
+        >
           <thead>
             <tr>
-              <th> Widget title </th>
-              <th> Plans assigned </th>
-              <th> Highlights </th>
-              <th> Sticky ATC </th>
-              <th> Card badge </th>
-              <th> Actions </th>
+              <th>Widget title</th>
+              <th>Plans assigned</th>
+              <th>Highlights</th>
+              <th>Sticky ATC</th>
+              <th>Card badge</th>
+              <th>Actions</th>
             </tr>
           </thead>
-          <tbody >
-              <tr style={{textAlign:"center"}}>
-                <td>Widgets <Badge tone='attention'>Default</Badge></td>
-                <td>0</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>...</td>
-              </tr>
-            </tbody>
+
+          <tbody>
+            <tr style={{ textAlign: "center" }}>
+              <td>
+                Widgets <Badge tone="attention">Default</Badge>
+              </td>
+
+              <td>0</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>...</td>
+            </tr>
+          </tbody>
         </table>
       </Card>
-
     </Page>
-  )
+  );
 }
 
-export default widgets
+export default Widgets;
