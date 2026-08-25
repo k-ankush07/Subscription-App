138b44ff36591447718a7b506eaa7ebf277816ab
k-ankush07 <ankush@ecombithub.com>
2026-08-25 10:28:23 +0530

    startt

--- extensions/subscription-portal/src/SubscriptionPortal.jsx
+++ extensions/subscription-portal/src/SubscriptionPortal.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useCallback, useRef } from "preact/hooks";
 import { COUNTRIES } from "../../../app/routes/utils/countries";
 
 const API_BASE =
-  "https://figure-commerce-depth-advertiser.trycloudflare.com";
+  "https://stewart-capabilities-indicator-thumb.trycloudflare.com";
 const PAGE_SIZE = 7;
 const CANCEL_REASONS = [
   "Too expensive",
