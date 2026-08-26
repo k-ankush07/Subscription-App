cb2aa062fe2c3944b314f04c99b8bf83db1e401c
k-ankush07 <ankush@ecombithub.com>
2026-08-26 10:18:47 +0530

    start

--- extensions/subscription-portal/src/SubscriptionPortal.jsx
+++ extensions/subscription-portal/src/SubscriptionPortal.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useCallback, useRef } from "preact/hooks";
 import { COUNTRIES } from "../../../app/routes/utils/countries";
 
 const API_BASE =
-  "https://stewart-capabilities-indicator-thumb.trycloudflare.com";
+  "https://franklin-declared-hereby-draw.trycloudflare.com";
 const PAGE_SIZE = 7;
 const CANCEL_REASONS = [
   "Too expensive",
