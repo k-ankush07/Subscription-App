933e47a22a205dc82b16978c9a7932a12fd8ce46
k-ankush07 <ankush@ecombithub.com>
2026-08-26 18:58:57 +0530

    update button

--- app/routes/app.subscription.$id.jsx
+++ app/routes/app.subscription.$id.jsx
@@ -441,7 +441,7 @@ async function rescheduleSingleCycle(admin, contractId, cycleIndex, isoDate) {
   return data?.data?.subscriptionBillingCycleScheduleEdit;
 }
 
-fwdwqdwdhwdvhefefedfdefs
+fwdwqdwdhwdvhefefedfdefssdxsad
 export async function action({ request, params }) {
   const formData = await request.formData();
   const type = formData.get("type");
