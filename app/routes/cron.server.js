15681136ef206b3fed129f27dde9f8dcc4c1950f
k-ankush07 <ankush@ecombithub.com>
2026-08-27 10:16:37 +0530

    ading

--- app/routes/cron.server.js
+++ app/routes/cron.server.js
@@ -7,9 +7,9 @@ export function startBillingCycleCron() {
   globalThis.__billingCronStarted = true;
   cron.schedule("*/5 * * * *", async () => {
     console.log("[cron] Running process-billing-cycles...");
-    console.log("process.env https://zum-barely-romance-johnson.trycloudflare.com")
+    console.log("process.env https://course-military-highs-require.trycloudflare.com")
     try {
-      const res = await fetch(`https://zum-barely-romance-johnson.trycloudflare.com/api/process-billing-cycles`, {
+      const res = await fetch(`https://course-military-highs-require.trycloudflare.com/api/process-billing-cycles`, {
         method: "POST",
         headers: { "x-cron-secret": process.env.CRON_SECRET },
       });
