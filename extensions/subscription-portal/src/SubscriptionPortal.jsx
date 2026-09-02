2124953f59341b022272b75c94a4dd90fe9128cc
k-ankush07 <ankush@ecombithub.com>
2026-09-02 10:18:14 +0530

    start

--- extensions/subscription-portal/src/SubscriptionPortal.jsx
+++ extensions/subscription-portal/src/SubscriptionPortal.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useCallback, useRef } from "preact/hooks";
 import { COUNTRIES } from "../../../app/routes/utils/countries";
 
 const API_BASE =
-  "https://stream-departments-basement-tire.trycloudflare.com";
+  "https://montana-tariff-latino-extends.trycloudflare.com";
 const PAGE_SIZE = 7;
 const CANCEL_REASONS = [
   "Too expensive",
